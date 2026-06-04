1068f641312c6ab48527aa6e8f70307b45a6b7b9
Axel Konrad <ak-li@siduction.org>
2026-06-04 15:32:13 +0200

    Fix wallpaper settings

--- xfce-desktop-settings.sh
+++ xfce-desktop-settings.sh
@@ -6,11 +6,23 @@
 # Run as user.
 #
 
+#------------------
 # DESKTOP (xfce4-desktop)
-## Same background for all workspaces
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor$(xrandr | grep -m1 " connected " | awk '{print $1}')/workspace0/last-image -s /usr/share/wallpapers/big-crime.png --create -t string
-xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-mode -s true --create -t bool
-xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-number -s 0 --create -t int
+## Find out the interface and the screen size.
+## Search for a suitable wallpaper file.
+## Set same background for all workspaces.
+Monitor=$(xrandr | grep -m1 " connected " | sed 's|+.*$||')
+Iface=$(awk '{print $1}' <<< $Monitor)
+SizeFile=$(awk '{print $'"$(wc -w <<< "$Monitor")"' ".png"}' <<< $Monitor)
+ImagePath="/usr/share/wallpapers/big-crime/contents/images/"
+if [ -f "$ImagePath$SizeFile" ]; then
+	xfconf-query -c xfce4-desktop -pn /backdrop/screen0/monitor"$Iface"/workspace0/last-image -t string -s "$ImagePath$SizeFile" --create
+else
+	xfconf-query -c xfce4-desktop -pn /backdrop/screen0/monitor"$Iface"/workspace0/last-image -t string -s /usr/share/wallpapers/big-crime.png --create
+	xfconf-query -c xfce4-desktop -pn /backdrop/screen0/monitor"$Iface"/workspace0/image-style -t uint -s 3 --create
+fi
+xfconf-query -c xfce4-desktop -pn /backdrop/single-workspace-mode -s true --create -t bool
+xfconf-query -c xfce4-desktop -pn /backdrop/single-workspace-number -s 0 --create -t int
 
 ## Transparent background for labeling the desktop icons
 xfconf-query -c xfce4-desktop -p /desktop-icons/label-background-color -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 --create
@@ -23,7 +35,8 @@ xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-trash -s false -
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -s true --create -t bool
 
 
-# Panel
+# -----------------
+# PANEL
 ## Panel-1, size and automatically adjust icon size
 xfconf-query -c xfce4-panel -p /panels/panel-1/size -t uint -s 40 --create
 xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -t uint -s 0 --create
@@ -36,7 +49,8 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -s false -t b
 xfconf-query -c xfce4-panel -p /plugins/plugin-6/icon-size -t uint -s 0 --create
 
 
-# Metadata siduction icons
+# -----------------
+# METADATA SIDUCTION ICONS
 ## Setting the metadata::xfce-exe-checksum attribute for the desktop icons
 cd /home/${USER}/Desktop/
 gio set -t string "siduction-manual.desktop" metadata::xfce-exe-checksum "$(sha256sum "siduction-manual.desktop" | awk '{print $1}')"
@@ -44,10 +58,23 @@ gio set -t string "siduction-irc.desktop" metadata::xfce-exe-checksum "$(sha256s
 gio set -t string "chroot-helper.desktop" metadata::xfce-exe-checksum "$(sha256sum "chroot-helper.desktop" | awk '{print $1}')" 2>/dev/null
 gio set -t string "calamares.desktop" metadata::xfce-exe-checksum "$(sha256sum "calamares.desktop" | awk '{print $1}')" 2>/dev/null
 
+
+# -----------------
+# TERMINAL (xfce4-terminal)
+## Setting color palette
+xfconf-query -c xfce4-terminal -pn /color-background -s \#000000 --create -t string
+xfconf-query -c xfce4-terminal -pn /color-foreground -s \#ffffff --create -t string
+xfconf-query -c xfce4-terminal -pn /color-bold-is-bright -s true --create -t bool
+xfconf-query -c xfce4-terminal -pn /color-bold-use-default -s true --create -t bool
+xfconf-query -c xfce4-terminal -pn /color-cursor-use-default -s true --create -t bool
+xfconf-query -c xfce4-terminal -pn /color-selection-use-default -s true --create -t bool
+xfconf-query -c xfce4-terminal -pn /color-palette -s \#000000\;\#cc0000\;\#4e9a06\;\#c4a000\;\#3465a4\;\#75507b\;\#06989a\;\#d3d7cf\;\#555753\;\#ef2929\;\#8ae234\;\#fce94f\;\#739fcf\;\#ad7fa8\;\#34e2e2\;\#eeeeec --create -t string
+xfconf-query -c xfce4-terminal -pn /tab-activity-color -s \#aa0000 --create -t string
+
 sleep 1
 
-if [ -f /home/${USER}/.config/autostart/Desktop-settings.desktop ]; then
-    rm /home/${USER}/.config/autostart/Desktop-settings.desktop
+if [ -f /home/"$USER"/.config/autostart/Desktop-settings.desktop ]; then
+    rm /home/"$USER"/.config/autostart/Desktop-settings.desktop
 fi
 
 exit 0
